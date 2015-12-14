673b8a6eae3ef2169d26e770a3ff1e297caa6fef
David Lukac <david.lukac@gmail.com>
2015-12-14 09:32:47 +0100

    Issue #3 - Add `build.properties` to downgrade SBT to `0.13.7` in order to fix `build.sbt` and be able to use `Scalatest`.

--- build.sbt
+++ build.sbt
@@ -2,8 +2,8 @@ name := "Advert of Code 2015"
 
 organization := "com.gaincore"
 
-version := "0.1.0"
+version := "v0.1.0"
 
-scalaVersion := "2.11.7"
+scalaVersion := "2.11.2"
 
 libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test"
--- src/test/scala/com.gaincore.adventofcode.tests/DispatchSpec.scala
+++ src/test/scala/com.gaincore.adventofcode.tests/DispatchSpec.scala
@@ -1,8 +1,12 @@
 package com.gaincore.adventofcode.tests
 
+import org.scalatest.{ShouldMatchers, FlatSpec}
+
 /**
   * Created by David Lukac on 13/12/15.
   */
-class DispatchSpec {
+class DispatchSpec extends FlatSpec with ShouldMatchers {
+
+  val test = "Some test is going to be here"
 
 }
